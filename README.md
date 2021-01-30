3102f157a925f0a45881a785dc07e9ef82b60f60
mcgillij <mcgillivray.jason@gmail.com>
2021-01-29 20:59:57 -0400

    add cpu_governor

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # cpu_governor
-p3status cpu_governor for i3
+py3status cpu_governor for i3
+
+## screenshot
+![py3status cpu_governor](/images/cpu_governor.png)
+
+## pre-reqs
+* i3
+* py3status
+
+## installation
+
+``` bash
+cp cpu_governor.py ~/.i3/py3status/
+```
+
+## configuration
+
+add the following line to your ~/.config/i3/i3status.conf
+
+``` bash
+order += "cpu_governor"
+```
+
+restart i3
---
+++ cpu_governor.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Module to report the CPU governor state in py3status bar
+"""
+
+
+class Py3status:
+    cache_timeout = 600
+    format = '💻: {status}'
+
+    def _get_cpu_status(self):
+        try:
+            status = self.py3.command_output(["cat", "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"])
+            return {'status': status.strip()}
+
+        except self.py3.CommandError as ce:
+            return len(ce.output.splitlines())
+
+    def cpu_governor_status(self):
+
+        status = self._get_cpu_status()
+        full_text = self.py3.safe_format(self.format, status)
+
+        return {
+            'full_text': full_text,
+            'cached_until': self.py3.time_in(self.cache_timeout)
+        }
+
+if __name__ == "__main__":
+    from py3status.module_test import module_test
+    module_test(Py3status)
