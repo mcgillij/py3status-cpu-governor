b1bdf0e5e6460168cb3de081dbf6f5696c296354
mcgillij <mcgillivray.jason@gmail.com>
2021-03-06 18:03:35 -0400

    reworking for pypi

---
+++ PKGBUILD
@@ -0,0 +1,26 @@
+# Maintainer: Jason McGillivray < mcgillivray dot jason at gmail dot com>
+
+
+pkgname=py3status-cpu-governor
+pkgdesc="Python daemon for controlling the fans on amdgpu cards"
+pkgver=0.1.0
+pkgrel=1
+arch=('any')
+license=('GPL2')
+depends=('python' 'py3status')
+makedepends=('python-setuptools')
+url="https://github.com/mcgillij/amdfan"
+source=("https://github.com/mcgillij/amdfan/releases/download/0.1.7/amdfan-0.1.7.tar.gz")
+md5sums=('c6c556f72fbe35a4bdb777754e8e41aa')
+
+build() {
+  cd "$srcdir/$pkgname-$pkgver"
+  python setup.py build
+}
+
+package() {
+  cd "$srcdir/$pkgname-$pkgver"
+  python setup.py install --prefix=/usr --root="$pkgdir"
+  mkdir -p "$pkgdir/usr/lib/systemd/system"
+  install -Dm644 amdfan/amdfan.service "$pkgdir/usr/lib/systemd/system/"
+} 
--- src/py3status_cpu_governor.py
+++ src/py3status_cpu_governor.py
@@ -3,15 +3,17 @@
 Module to report the CPU governor state in py3status bar
 """
 
-
 class Py3status:
-    cache_timeout = 600
-    format = '💻: {status}'
+    cache_timeout = 6
+    format = "💻: {status}"
 
     def _get_cpu_status(self):
         try:
-            status = self.py3.command_output(["cat", "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"])
-            return {'status': status.strip()}
+            status = self.py3.command_output(
+                ["cat",
+                    "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"]
+            )
+            return {"status": status.strip()}
 
         except self.py3.CommandError as ce:
             return len(ce.output.splitlines())
@@ -22,9 +24,13 @@ class Py3status:
         full_text = self.py3.safe_format(self.format, status)
 
         return {
-            'full_text': full_text,
-            'cached_until': self.py3.time_in(self.cache_timeout)
+            "full_text": full_text,
+            "cached_until": self.py3.time_in(self.cache_timeout),
         }
+
+def run():
+    print("You need to add this module to your i3status.conf")
+
 
 if __name__ == "__main__":
     from py3status.module_test import module_test
